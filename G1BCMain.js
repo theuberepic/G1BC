27ec8ec70e656c574a33c1102c86edb6be79b51c
TheUberEpic <theuberepic@hotmail.co.uk>
2014-01-14 17:05:07 +0000

    Basic structure of G1BC app

---
+++ G1BCMain.js
@@ -0,0 +1,14 @@
+//////////////////////////////////////
+////// GOOPER1 BUILDING CREATOR //////
+//////////////////////////////////////
+          //  MAIN JS FILE  //
+         // LICENCE: GPL 2 //
+        ////////////////////
+        
+$(document).ready(function() {
+  
+  
+// Check if we are running under Node.js 
+window.ON_NODEJS = (typeof window === 'undefined') ? true : false;
+  
+});
